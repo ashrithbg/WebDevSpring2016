74632704623f7c9c3fb6a6e21ecbedb4520655a0
Ashrith Bangi <ashrith@Ashriths-MacBook-Pro.local>
2016-02-22 06:19:48 -0500

    Added routing for pages

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -1,3 +1,48 @@
-/**
- * Created by ashrith on 2/22/16.
- */
+(function(){
+    angular.module("FormBuilderApp")
+        .config(function($routeProvider){
+
+            $routeProvider.
+                when("/",{
+                templateUrl:"views/home/home.view.html",
+                controller: "HomeController"
+            })
+                .when("/profile",{
+                templateUrl:"views/users/profile/profile.view.html",
+                controller: "ProfileController"
+                })
+                .when("/admin",{
+                templateUrl:"views/admin/admin.view.html",
+                controller: "AdminController"
+                })
+                .when("/forms/forms",{
+                templateUrl:"views/forms/forms.view.html",
+                controller: "FormController"
+                })
+                .when("/forms/fields",{
+                    templateUrl:"views/forms/fields.view.html",
+                    controller: "FieldsController"
+                })
+                .when("/users/login",{
+                templateUrl:"views/users/login.view.html",
+                controller: "LoginController"
+            }).when("/users/register",{
+                templateUrl:"views/users/register.view.html",
+                controller: "RegisterController"
+            }).when("/users/profile",{
+                    templateUrl:"views/users/profile.view.html",
+                    controller: "ProfileController"
+            }).otherwise({
+                redirectTo: "/"
+            });
+
+
+
+
+        })
+
+
+
+
+
+})();
--- public/assignment/views/home/home.controller.js
+++ public/assignment/views/home/home.controller.js
@@ -1,3 +1,13 @@
 /**
  * Created by ashrith on 2/21/16.
  */
+(function(){
+    angular.
+        module("FormBuilderApp")
+        .controller("HomeController",HomeController);
+        function HomeController($scope){
+
+        }
+
+
+})();
